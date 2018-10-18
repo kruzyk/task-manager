d942a6e26a8dd3f7857e33c095e0cf6922821f35
kruzyk <kruzyk@gmail.com>
2018-10-18 12:06:51 +0200

    install firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDPZaSWrcdyrtAn7MSiWglvo0dRAoAgmaw",
+  authDomain: "task-manager-6d83d.firebaseapp.com",
+  databaseURL: "https://task-manager-6d83d.firebaseio.com",
+  projectId: "task-manager-6d83d",
+  storageBucket: "task-manager-6d83d.appspot.com",
+  messagingSenderId: "557601706684"
+};
+firebase.initializeApp(config);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
