3765da243c7d325d2e477e8791fb5e5d86951bdd
kruzyk <kruzyk@gmail.com>
2018-10-19 11:40:18 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,160 +1,117 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 
+const buttonNames = ["All", "Completed", "Active"];
+const filters = {
+  All: () => true,
+  Completed: task => task.isDone,
+  Active: task => !task.isDone
+};
+
 class App extends Component {
   state = {
     taskTitleAdd: "",
     taskTitleEdit: "",
-    selectedStatus: "All",
-    tasks: [
-      {
-        id: 1,
-        title: "Read pdfs",
-        isDone: false,
-        inEdit: false
-      },
-      {
-        id: 2,
-        title: "Learn React",
-        isDone: true,
-        inEdit: false
-      },
-      {
-        id: 3,
-        title: "Go to sleep",
-        isDone: false,
-        inEdit: false
-      }
-    ]
-  };
-
-  setNewTask = snapshot => {
-    this.setState({
-      tasks: Object.entries(snapshot.val() || {})
-        .map(([id, other]) => ({ id, ...other }))
-        .reverse()
-    });
+    activeButtonName: "All",
+    tasks: []
   };
 
   componentDidMount() {
-    this.componentIsMount = true;
-    this.ref = firebase.database().ref("/publicTodos/");
-    this.ref.on("value", this.setNewTask);
-  }
-
-  componentWillUnmount() {
-    this.componentIsMount = false;
-    if (this.ref) {
-      this.ref.off("value", this.setNewTask);
-    }
-  }
-
-  addTask = title => {
     firebase
       .database()
-      .ref("publicTodos/")
-      .push({
-        title: title,
-        isDone: false,
-        inEdit: false
+      .ref("/publicTodos")
+      .on("value", snapshot => {
+        const tasks =
+          // zamieniamy obiekt na tablicę tablic dwuelementowych [id, wartość]
+          Object.entries(snapshot.val() || {})
+            // zamieniamy tę tablicę na tablicę obiektów złożonych z atrybutu id,
+            // będącego pierwszym elementem z tablicy dwuelementowej oraz wszystkich
+            // atrybutów drugiego elementu
+            .map(([id, value]) => ({ id, ...value }))
+            // odwracamy tablicę, bo chcemy mieć najnowsze taski na górze
+            .reverse();
+
+        // czyścimy pole formularza
+        this.setState({
+          tasks
+        });
       });
+  }
+
+  makeHandleChange = fieldName => event => {
     this.setState({
-      taskTitleAdd: ""
+      [fieldName]: event.target.value
     });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    if (this.state.taskTitleAdd === "") {
-      return;
-    }
-    this.addTask(this.state.taskTitleAdd);
-  };
+    firebase
+      .database()
+      .ref("/publicTodos")
+      .push({
+        title: this.state.taskTitleAdd,
+        isDone: false,
+        inEdit: false
+      });
 
-  handleChange = event => {
     this.setState({
-      taskTitleAdd: event.target.value
+      taskTitleAdd: ""
     });
   };
 
-  handleTaskDone = taskId => {
-    this.setState({
-      tasks: this.state.tasks.map(
-        task =>
-          taskId !== task.id
-            ? task
-            : {
-                ...task,
-                isDone: !task.isDone
-              }
-      )
-    });
+  makeHandleDestroyClick = taskId => event => {
+    firebase
+      .database()
+      .ref("publicTodos")
+      .child(taskId)
+      .remove();
   };
 
-  removeTask = taskId => {
-    this.setState({
-      tasks: this.state.tasks.filter(task => taskId !== task.id)
-    });
+  makeHandleCheckboxChange = task => event => {
+    firebase
+      .database()
+      .ref("publicTodos")
+      .child(task.id)
+      .update({
+        isDone: !task.isDone
+      });
   };
 
-  handleTaskEditStatus = taskId => {
-    const taskToEdit = this.state.tasks.find(task => task.id === taskId);
-    this.setState({
-      taskTitleEdit: taskToEdit.title,
-      tasks: this.state.tasks.map(
-        task =>
-          taskId !== task.id
-            ? task
-            : {
-                ...task,
-                inEdit: !task.inEdit
-              }
-      )
-    });
-  };
+  makeHandleEnterEditMode = task => event => {
+    firebase
+      .database()
+      .ref("publicTodos")
+      .child(task.id)
+      .update({
+        inEdit: true
+      });
 
-  editTask = taskId => {
-    this.setState({
-      tasks: this.state.tasks.map(
-        task =>
-          taskId !== task.id
-            ? task
-            : {
-                ...task,
-                title: this.state.taskTitleEdit,
-                inEdit: !task.inEdit
-              }
-      )
-    });
-  };
-
-  handleEdit = event => {
-    event.preventDefault();
-    this.setState({ taskTitleEdit: event.target.value });
-  };
-
-  handleSelectedFilter = status => {
-    this.setState({
-      selectedStatus: status
-    });
+      this.setState({
+        taskTitleEdit: task.title
+      })
   };
 
   makeHandleInputKeypress = taskId => event => {
     if (event.key !== "Enter") {
       return;
     }
-    this.setState({
-      tasks: this.state.tasks.map(
-        task =>
-          task.id !== taskId
-            ? task
-            : {
-                ...task,
-                title: this.state.taskTitleEdit,
-                inEdit: false
-              }
-      )
-    });
+    this.updateTaskTitle(taskId);
+  };
+
+  makeHandleMouseLeave = taskId => event => {
+    this.updateTaskTitle(taskId);
+  };
+
+  updateTaskTitle = taskId => {
+    firebase
+      .database()
+      .ref("publicTodos")
+      .child(taskId)
+      .update({
+        title: this.state.taskTitleEdit,
+        inEdit: false
+      });
   };
 
   render() {
@@ -168,7 +125,7 @@ class App extends Component {
                 className="new-todo"
                 placeholder="What needs to be done?"
                 value={this.state.taskTitleAdd}
-                onChange={this.handleChange}
+                onChange={this.makeHandleChange("taskTitleAdd")}
               />
             </form>
           </header>
@@ -177,47 +134,37 @@ class App extends Component {
             <label for="toggle-all" />
             <ul className="todo-list">
               {this.state.tasks
-                .filter(
-                  task =>
-                    this.state.selectedStatus === "All"
-                      ? true
-                      : this.state.selectedStatus === "Active"
-                        ? task.isDone === false
-                        : task.isDone === true
-                )
+                .filter(filters[this.state.activeButtonName])
                 .map(task => (
                   <li
-                    className={
-                      task.isDone
-                        ? "completed"
-                        : task.inEdit
-                          ? "editing"
-                          : ""
-                    }
                     key={task.id}
+                    className={
+                      task.inEdit ? "editing" : task.isDone ? "completed" : ""
+                    }
                   >
                     <div className="view">
                       <input
                         className="toggle"
                         type="checkbox"
-                        onClick={() => this.handleTaskDone(task.id)}
+                        checked={task.isDone}
+                        onChange={this.makeHandleCheckboxChange(task)}
                       />
                       <label
-                        onDoubleClick={() => this.handleTaskEditStatus(task.id)}
+                        onDoubleClick={this.makeHandleEnterEditMode(task)}
                       >
                         {task.title}
                       </label>
                       <button
                         className="destroy"
-                        onClick={() => this.removeTask(task.id)}
+                        onClick={this.makeHandleDestroyClick(task.id)}
                       />
                     </div>
                     <input
                       className="edit"
                       value={this.state.taskTitleEdit}
-                      onChange={this.handleEdit}
                       onKeyPress={this.makeHandleInputKeypress(task.id)}
-                      onMouseLeave={() => this.editTask(task.id)}
+                      onChange={this.makeHandleChange("taskTitleEdit")}
+                      onMouseLeave={this.makeHandleMouseLeave(task.id)}
                     />
                   </li>
                 ))}
@@ -225,49 +172,29 @@ class App extends Component {
           </section>
           <footer className="footer">
             <span className="todo-count">
-              <strong>
-                {this.state.tasks.filter(task => task.isDone !== true).length}
-              </strong>
+              <strong>{this.state.tasks.filter(filters.Active).length}</strong>
               <span> </span>
               <span>items</span>
               <span> left</span>
             </span>
             <ul className="filters">
-              <li>
-                <a
-                  href="#/"
-                  className={
-                    this.state.selectedStatus === "All" ? "selected" : ""
-                  }
-                  onClick={() => this.handleSelectedFilter("All")}
-                >
-                  All
-                </a>
-              </li>
-              <span> </span>
-              <li>
-                <a
-                  href="#/active"
-                  className={
-                    this.state.selectedStatus === "Active" ? "selected" : ""
-                  }
-                  onClick={() => this.handleSelectedFilter("Active")}
-                >
-                  Active
-                </a>
-              </li>
-              <span> </span>
-              <li>
-                <a
-                  href="#/completed"
-                  className={
-                    this.state.selectedStatus === "Completed" ? "selected" : ""
-                  }
-                  onClick={() => this.handleSelectedFilter("Completed")}
-                >
-                  Completed
-                </a>
-              </li>
+              {buttonNames.map(buttonName => (
+                <li>
+                  <a
+                    href="#/"
+                    className={
+                      this.state.activeButtonName === buttonName
+                        ? "selected"
+                        : ""
+                    }
+                    onClick={() =>
+                      this.setState({ activeButtonName: buttonName })
+                    }
+                  >
+                    {buttonName}
+                  </a>
+                </li>
+              ))}
             </ul>
             <button className="clear-completed">Clear completed</button>
           </footer>
