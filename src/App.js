0fc2c11a407b0ac2578262e7a4e08ab6b1e179b4
kruzyk <kruzyk@gmail.com>
2018-10-19 12:13:15 +0200

    private todos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react'
-import Tasks from './Tasks'
-import firebase from 'firebase'
+import React, { Component } from "react";
+import Tasks from "./Tasks";
+import firebase from "firebase";
 
 class App extends Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("bartosz.cytrowski+foo1@gmail.com", "test1234");
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
   render() {
     return (
       <div>
         <Tasks firebaseRef={firebase.database().ref("publicTodos")} />
+        {this.state.user !== null && (
+          <Tasks
+            firebaseRef={firebase
+              .database()
+              .ref(`privateTodos/${this.state.user.uid}`)}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
